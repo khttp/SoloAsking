b4b9b09d39e5b16ca5f4ff3c8842342b64b1f84b
Abd El-rahman Khattab <abdelrahmankhattab9999@gmail.com>
2025-04-23 18:51:16 +0000

    enable the icon

--- src/app/ClientHeader.tsx
+++ src/app/ClientHeader.tsx
@@ -21,20 +21,16 @@ const playfairDisplay = Playfair_Display({
 const ClientHeader = () => {
   const pathname = usePathname();
   const {user} = useAuth();
-  const [isClient, setIsClient] = useState(false);
-  const {theme, setTheme} = useTheme();
   const [mounted, setMounted] = useState(false);
-
+  const {theme, setTheme} = useTheme();
 
   useEffect(() => {
-    setIsClient(true);
     setMounted(true);
-
   }, []);
 
-    if (!mounted) {
-        return null;
-    }
+  if (!mounted) {
+    return null;
+  }
 
   const getAvatarInitials = () => {
     if (user?.displayName) {
@@ -72,48 +68,44 @@ const ClientHeader = () => {
             >
               Questions
             </Link>
-            {isClient && (
-              <>
-                {user ? (
-                  <div className="flex items-center space-x-2">
-                    <Link
-                      href="/profile"
-                      className={`text-foreground hover:text-primary ${pathname === '/profile' ? 'text-orange-500' : ''
-                        }`}
-                    >
-                      <Avatar className="w-8 h-8">
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback>{getAvatarInitials()}</AvatarFallback>
-                      </Avatar>
-                    </Link>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-                    >
-                      {theme === 'light' ? <Moon /> : <Sun />}
-                      <span className="sr-only">Toggle theme</span>
-                    </Button>
-                  </div>
-                ) : (
-                  <div className="flex items-center space-x-2">
-                    <Link
-                      href="/login"
-                      className={`text-foreground hover:text-primary ${pathname === '/login' ? 'text-orange-500' : ''}`}
-                    >
-                      Login\Register
-                    </Link>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-                    >
-                      {theme === 'light' ? <Moon /> : <Sun />}
-                      <span className="sr-only">Toggle theme</span>
-                    </Button>
-                  </div>
-                )}
-              </>
+            {user ? (
+              <div className="flex items-center space-x-2">
+                <Link
+                  href="/profile"
+                  className={`text-foreground hover:text-primary ${pathname === '/profile' ? 'text-orange-500' : ''
+                    }`}
+                >
+                  <Avatar className="w-8 h-8">
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>{getAvatarInitials()}</AvatarFallback>
+                  </Avatar>
+                </Link>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+                >
+                  {theme === 'light' ? <Moon /> : <Sun />}
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </div>
+            ) : (
+              <div className="flex items-center space-x-2">
+                <Link
+                  href="/login"
+                  className={`text-foreground hover:text-primary ${pathname === '/login' ? 'text-orange-500' : ''}`}
+                >
+                  Login\Register
+                </Link>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+                >
+                  {theme === 'light' ? <Moon /> : <Sun />}
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </div>
             )}
           </div>
         </nav>
